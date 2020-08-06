0b0b020136f94131fad12ca6e5fc4a2cd429cafc
Somin Kang <somin.kang.91@gmail.com>
2020-08-05 20:11:58 -0700

    update structure

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var path = require('path');
+
+
+
+
